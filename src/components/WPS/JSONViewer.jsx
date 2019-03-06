63119f240584683975731a2f3896a321e5236b7b
Maarten Plieger <maartenplieger@gmail.com>
2019-03-06 11:31:40 +0100

    Added compute node selector

---
+++ src/components/WPS/JSONViewer.jsx
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import ReactJson from 'react-json-view';
+export default class JSONViewer extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      url: props.url,
+      data: null
+    };
+  }
+
+  componentDidMount () {
+    axios({
+      method: 'get',
+      url: this.state.url,
+      withCredentials: true
+    }).then(src => {
+      this.setState({ data: src.data });
+    }).catch((e) => {
+      console.error(e);
+    });
+  }
+
+  render () {
+    return (<div className='wpsOutputComponentContainer'>
+      <ReactJson style={{ fontSize:'10px', lineHeight:'12px' }} src={this.state.data} />
+    </div>);
+  }
+}
+
+JSONViewer.propTypes = {
+  url: PropTypes.string
+};
