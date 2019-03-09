ccdd084e2e60ae17b7c6e56a064151f4feb2d772
Niels Drost <n.drost@esciencecenter.nl>
2019-03-09 21:21:41 +0100

    Update configDefault.js
    Typo

--- src/static/configDefault.js
+++ src/static/configDefault.js
@@ -2,5 +2,5 @@ const configDefault = {};
 configDefault.backendHost = 'https://portal.c3s-magic.eu';
 configDefault.adagucViewerURL = 'http://portal.c3s-magic.eu/adaguc-viewer/';
 configDefault.staticWMS='https://portal.c3s-magic.eu/backend/wms';
-config.dataURL = 'https://portal-dev.c3s-magic.eu/data';
+configDefault.dataURL = 'https://portal-dev.c3s-magic.eu/data';
 module.exports = configDefault;
